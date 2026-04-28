8e4c1ce071551b6078d5c208d0d69d626a1ac2a0
kartikmanimuthu <kartikmanimuthu@gmail.com>
2026-04-28 19:30:59 +0530

    chore(scheduler): Remove deprecated lambda/scheduler and vector_processor implementations

--- lambda/vector_processor/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Handler } from 'aws-lambda';
-
-export const handler: Handler = async (event) => {
-    console.log('Vector processor Lambda invoked', { event });
-    return { statusCode: 200, body: 'OK' };
-};
